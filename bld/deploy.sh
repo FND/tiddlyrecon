cce876029672f90c14f90d8a33c905d8783b470d
FND <fnd@oda.(none)>
2009-12-01 15:08:19 +0000

    added deploy script
    While currently rather crude, this is useful for demo purposes.

---
+++ bld/deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -x
+
+host="collab.tiddlywiki.org"
+base_dir="/var/www/$host/htdocs"
+
+if [ ! -d bld ]; then # XXX: unnecessary!?
+	echo "ERROR: script must be executed from repository root"
+	exit 1
+fi
+
+cd `dirname $0`
+scp TiddlyRecon.zip $host:~/tmp/ && \
+ssh $host "sudo rm -rf $base_dir/TiddlyRecon ~/tmp/TiddlyRecon; " \
+	"cd ~/tmp && unzip -d TiddlyRecon TiddlyRecon.zip && " \
+	"sudo mv TiddlyRecon $base_dir/ && " \
+	"echo 'tiddlyweb.host = \"/wiki\";' > $base_dir/TiddlyRecon/scripts/config.js"
