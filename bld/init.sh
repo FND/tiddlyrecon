6d63ec5727e77ced3503fe8fe4b4e34a51d6a245
FND <fnd@oda.(none)>
2009-12-03 15:05:16 +0000

    added jquery-json to initialization script and index.html
    Ideally, this would be handled by chrjs...

--- bld/init.sh
+++ bld/init.sh
@@ -10,3 +10,5 @@ fi
 git submodule update --init
 curl -o "scripts/jquery.min.js" \
 	"http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js"
+curl -o "scripts/jquery-json.min.js" \
+	"http://jquery-json.googlecode.com/files/jquery.json-2.2.min.js"
