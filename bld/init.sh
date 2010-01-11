7df03f551d7665f02fcbeb82bb1495dcc51cd7fd
FND <fnd@oda.(none)>
2010-01-11 17:15:44 +0000

    added chrjs as regular dependency
    There's no reason to use submodules for this.

--- bld/init.sh
+++ bld/init.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env sh
 
-# pull in external dependencies
+# pulls in external dependencies
 
 if [ ! -d bld ]; then
 	echo "ERROR: script must be executed from repository root"
 	exit 1
 fi
 
-git submodule update --init
 curl -o "scripts/jquery.min.js" \
 	"http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js"
 curl -o "scripts/jquery-json.min.js" \
 	"http://jquery-json.googlecode.com/files/jquery.json-2.2.min.js"
+curl -o "scripts/chrjs.js" \
+	"http://github.com/tiddlyweb/chrjs/raw/master/main.js"
 curl -o "scripts/util.js" \
 	"http://github.com/FND/jquery/raw/master/util.js"
